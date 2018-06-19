4ec5a497ca1682a115015b0af9d921da48c03883
Natia94 <natiakhoma@gmail.com>
2018-06-19 16:08:25 -0400

    updated Blog

--- js/init.js
+++ js/init.js
@@ -6,3 +6,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
